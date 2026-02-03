be0dba9e2028382c950fc24e820cdc82d1b05fe1
Angela <166150697+anggeching@users.noreply.github.com>
2026-02-03 19:40:31 +0800

    cant input amount

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 require('dotenv').config();
+
+//declare routes and middleware 
 const express = require('express');
-const userRoutes = require('./routes/userRoutes');
-const googleRoutes = require('./routes/googleRoutes');
+const userRoutes = require('./routes/userRoutes'); //user-related routes
+const googleRoutes = require('./routes/googleRoutes'); //google OAuth-related routes
 
 const app = express();
 app.use(express.json()); // built-in middleware to parse JSON bodies
+
+// Prefixes: grouped by your user flow
 app.use('/api', userRoutes);
 app.use('/auth', googleRoutes);
 
